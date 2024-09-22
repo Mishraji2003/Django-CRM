304d88205f7798083ffbb273519b57d9631019d4
Gaurav Mishra <gauravmishra250103@gmail.com>
2024-09-22 12:21:13 +0530

    registration

--- CRUD/views.py
+++ CRUD/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 def home(request):
     if request.method == 'POST':
@@ -24,3 +25,4 @@ def logout_user(request):
 
 def register_user(request):
     return render(request, 'register.html', {})
+
